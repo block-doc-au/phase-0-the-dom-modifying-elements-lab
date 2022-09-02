8e6a71dd74142111a9c9d443a1e44693816524cf
block-doc-au <neilc@cns-gc.com.au>
2022-09-02 16:18:56 +1000

    NC Completed JS DOM NOdes lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute ('id', 'victory');
+
+newHeader.innerHTML = 'neilc is the champion';
+
+
